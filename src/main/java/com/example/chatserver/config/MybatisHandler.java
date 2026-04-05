b4dff222fe6a632b53de8d956bf77a6454bca676
hasya <Nedio7@qq.com>
2026-04-05 17:01:43 +0800

    MybatisHandler

--- src/main/java/com/example/chatserver/config/MybatisHandler.java
+++ src/main/java/com/example/chatserver/config/MybatisHandler.java
@@ -14,13 +14,13 @@ public class MybatisHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        this.setFieldValByName("createTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()), metaObject);
-        this.setFieldValByName("updateTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()), metaObject);
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.setFieldValByName("updateTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
     }
 
 }
