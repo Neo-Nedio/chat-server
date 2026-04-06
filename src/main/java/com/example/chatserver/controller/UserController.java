a2dac2c8d70709b3c32374411d98f68d597e776c
hasya <Nedio7@qq.com>
2026-04-06 17:06:13 +0800

    user

--- src/main/java/com/example/chatserver/controller/UserController.java
+++ src/main/java/com/example/chatserver/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.chatserver.controller;
 import cn.hutool.json.JSONObject;
 import com.example.chatserver.annotation.Userid;
 import com.example.chatserver.dto.UserDto;
+import com.example.chatserver.exception.BaseException;
+import com.example.chatserver.service.FriendService;
 import com.example.chatserver.service.UserService;
 import com.example.chatserver.utils.MinioUtil;
 import com.example.chatserver.utils.ResultUtil;
@@ -11,9 +13,14 @@ import com.example.chatserver.vo.user.UpdateVo;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 
@@ -24,6 +31,9 @@ public class UserController {
 
     @Resource
     UserService userService;
+
+    @Resource
+    FriendService friendService;
 
     @Resource
     MinioUtil minioUtil;
@@ -78,5 +88,23 @@ public class UserController {
         url += "?t=" + System.currentTimeMillis();
         userService.updateUserPortrait(userId, url);
         return ResultUtil.Succeed(url);
+    }
+
+    /**
+     * 获取图片内容
+     */
+    @GetMapping("/get/img")
+    public ResponseEntity<InputStreamResource> getFile(@Userid String userId,
+                                                       @RequestHeader("targetId") String targetId,
+                                                       @RequestHeader("fileName") String fileName) {
+        boolean isFriend = friendService.isFriend(userId, targetId);
+        if (!isFriend && !userId.equals(targetId)) {
+            throw new BaseException("双方非好友");
+        }
+        InputStream inputStream = minioUtil.getObject(targetId + "/img/" + fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(new InputStreamResource(inputStream));
     }
 }
