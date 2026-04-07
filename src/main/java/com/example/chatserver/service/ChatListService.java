decc5f592b64e0a36033773fad0cfad9511f87b6
hasya <Nedio7@qq.com>
2026-04-07 12:29:35 +0800

    获取目标对话

--- src/main/java/com/example/chatserver/service/ChatListService.java
+++ src/main/java/com/example/chatserver/service/ChatListService.java
@@ -26,4 +26,6 @@ public interface ChatListService extends IService<ChatList> {
     boolean topChatList(String userId, TopChatListVo topChatListVo);
 
     int unread(String userId);
+
+    ChatList getChatListByUserIdAndFromId(String userId, String fromId);
 }
