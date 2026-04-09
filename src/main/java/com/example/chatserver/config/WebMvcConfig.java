4363e971fa4d1187af2e5febd8e5c623038100ef
hasya <Nedio7@qq.com>
2026-04-09 21:04:34 +0800

    cors

--- src/main/java/com/example/chatserver/config/WebMvcConfig.java
+++ src/main/java/com/example/chatserver/config/WebMvcConfig.java
@@ -1,6 +1,11 @@
 package com.example.chatserver.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +19,28 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) { //Spring 维护的解析器列表
         resolvers.add(new UserInfoArgumentResolver()); //将自定义解析器添加到列表中
+    }
+
+    @Bean
+    //跨域请求过滤器的配置
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        //创建 CORS 配置源，用于管理不同路径的 CORS 规则
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        //创建 CORS 配置对象
+        CorsConfiguration config = new CorsConfiguration();
+        //允许携带认证信息（如 Cookie、Authorization 头）
+        config.setAllowCredentials(true);
+        //允许所有来源的请求访问。
+        config.addAllowedOriginPattern("*");
+        //允许所有请求头
+        config.addAllowedHeader("*");
+        //允许所有 HTTP 方法
+        config.addAllowedMethod("*");
+        //将 CORS 配置应用到所有路径（/** 表示所有路径）
+        source.registerCorsConfiguration("/**", config);
+        //创建过滤器注册 Bean，并设置优先级为 0（数值越小优先级越高
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
