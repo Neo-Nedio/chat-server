34f1ba55c18bdfe0464cd0e2c1e171ad08f7fe88
hasya <Nedio7@qq.com>
2026-04-18 15:54:57 +0800

    已解散的群聊无法查询，非群聊成员也可以查询群聊信息

--- src/main/java/com/example/chatserver/mapper/ChatGroupMapper.java
+++ src/main/java/com/example/chatserver/mapper/ChatGroupMapper.java
@@ -13,14 +13,14 @@ public interface ChatGroupMapper extends BaseMapper<ChatGroup> {
 
     @Select("SELECT cg.*, cgm.`group_remark` AS `group_remark` FROM `chat_group` AS cg " +
             "LEFT JOIN `chat_group_member` AS cgm ON cg.`id` = cgm.`chat_group_id` AND cgm.`user_id` = #{userId} " +
-            "WHERE cgm.`user_id` = #{userId}")
+            "WHERE cgm.`user_id` = #{userId} AND cg.`status` = 1")
     //获得自己的群聊列表
     List<ChatGroup> getList(String userId);
 
     @Select("<script>" +
             "SELECT cg.*, cgm.`group_remark` AS `group_remark` FROM `chat_group` AS cg " +
             "LEFT JOIN `chat_group_member` AS cgm ON cg.`id` = cgm.`chat_group_id` AND cgm.`user_id` = #{userId} " +
-            "WHERE cgm.`user_id` = #{userId} " +
+            "WHERE cgm.`user_id` = #{userId} AND cg.`status` = 1" +
             "<if test=\"search != null and search != ''\">" +
             "AND (cg.`name` LIKE CONCAT('%', #{search}, '%') " +
             "OR cg.`chat_group_number` LIKE CONCAT('%', #{search}, '%') " +
@@ -30,9 +30,9 @@ public interface ChatGroupMapper extends BaseMapper<ChatGroup> {
     //搜索自己的群聊列表
     List<ChatGroup> getListFromSearch(String userId, String search);
 
-    @Select("SELECT cg.*,cgm.`group_name`,cgm.`group_remark` from `chat_group` as cg " +
-            "LEFT JOIN `chat_group_member` as cgm on cg.`id` = cgm.`chat_group_id` " +
-            "where cg.`id` = #{chatGroupId} AND cgm.`user_id`=#{userId} ")
+    @Select("SELECT cg.*, cgm.`group_name`, cgm.`group_remark` FROM `chat_group` AS cg " +
+            "LEFT JOIN `chat_group_member` AS cgm ON cg.`id` = cgm.`chat_group_id` AND cgm.`user_id` = #{userId} " +
+            "WHERE cg.`id` = #{chatGroupId} AND cg.`status` = 1")
     @ResultMap("ChatGroupDetailsDtoResultMap")
     ChatGroupDetailsDto detailsChatGroup(String userId, String chatGroupId);
 }
